c3bee1906d32480631bb251fa8988c860cc4d16e
Rafael Goncalves <rafaelgoncalves@riseup.net>
2019-05-10 22:43:41 -0300

    Done.

--- src/main.c
+++ src/main.c
@@ -13,12 +13,12 @@ int is_prime(int n)
 {
   int i = 5;
   if (n <= 3)
-    return (n > 1);
+    return (n > 1); /* 0 0 1 */
   else if (n % 2 == 0 || n % 3 == 0)
     return 0;
-  while (i * i <= n)
+  while (i * i <= n) /* Until i = sqrt(n). */
   {
-    if (n & i == 0 || n % (i + 2) == 0)
+    if (n % i == 0 || n % (i + 2) == 0) /* Divisible by i or i+2. */
       return 0;
     i += 6;
   }
@@ -47,24 +47,24 @@ int * alloc_mmap(int n)
 int main() {
   pid_t pid[N];
   int *aux[N];
-  int *k;
   int nums[MAX];
   int n, ans, counter = 0;
   char c;
 
   do {
     scanf("%d", &nums[counter++]);
-    /* printf("%d ", nums[counter - 1]); */
   }
   while((c = getchar()) != '\n');
 
-  k = alloc_mmap(1);
+  /* Create mmap vectors for each process. */
+  for (int i = 0; i < N; i++)
+    aux[i] = alloc_mmap(counter/N + 1);
 
   for (int i = 0; i < N; i++)
   {
     pid[i] = fork();
-    aux[i] = alloc_mmap(counter/N + 1);
 
+    /* Child process. */
     if (pid[i] == 0)
     {
       for (int j = 0; j < counter/N + 1; j++)
@@ -72,30 +72,24 @@ int main() {
         if ((i*(counter/N + 1) + j) < counter)
           n = is_prime(nums[i*(counter/N + 1) + j]);
         else
-          n = 0;
+          n = 0; /* Empty slot. */
         aux[i][j] = n;
-        (*k) += n;
-        /* printf("%d(%d) ", nums[i*(counter/N + 1) + j], aux[i][j]); */
       }
-      /* printf("\n"); */
       exit(0);
     }
   }
 
+  /* Wait all processes to finish. */
   for (int i = 0; i < N; i++)
     waitpid(pid[i], NULL, 0);
 
+  /* Sum boolean vectors into ans. */
   ans = 0;
   for (int i = 0; i < N; i++)
     for (int j = 0; j < counter/N + 1; j++)
-    {
       ans += aux[i][j];
-      /* printf("%d ", aux[i][j]); */
-    }
-  /*  */
-  /* printf("\n"); */
 
-  printf("%d\n", (*k));
+  printf("%d\n", ans);
 
   return 0;
 }
